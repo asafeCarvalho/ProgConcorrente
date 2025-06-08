fff92f2594db733e1ae280364e8041922a421a49
asafe <asafe.dsc@gmail.com>
2025-06-08 13:30:00 -0300

    adicionand script

--- trabalhoFinal/johnson/concorrente.cpp
+++ trabalhoFinal/johnson/concorrente.cpp
@@ -1,15 +1,21 @@
 #include <bits/stdc++.h>
+#include "timer.h"
 
 
-typedef std::vector<std::vector<long long>> matR;
+typedef std::vector<std::vector<long long*>> matR;
 typedef long long ll;
 
 pthread_mutex_t mutex;
 
-matR matrizDoGrafo;
 ll totalVertices;
-int proximoVertice;
+ll totalArestas;
+
+
+int proximoVertice; //variavel para threads
+
 ll** distanciasFinais;
+std::vector<std::vector<long long*>> adjacencia; //adjacencia do grafo
+std::vector<std::vector<ll*>> adjacenciaRebalanceada;
 
 std::vector<ll> distanciasFalsas;
 int numThtreads;
@@ -29,37 +35,50 @@ void* Malloc(int tamanho) {
 }
 
 
-std::vector<std::vector<long long>> lerInput(int totalVertices, int totalArestas) {
-    std::vector<std::vector<long long>> adjacencia;
+void lerInput(FILE* arq) {
+    adjacencia;
     for (int i = 0; i < totalArestas; i++) {
         int u, v, peso;
-        scanf("%d %d %d\n", &u, &v, &peso);
-        adjacencia.push_back(std::vector<ll> {u, v, peso});
+        fscanf(arq, "%d %d %d\n", &u, &v, &peso);
+        ll* aresta = (ll*) Malloc(sizeof(ll) * 2);
+        aresta[0] = v; aresta[1] = peso;
+
+        adjacencia[u].push_back(aresta);
     }
-    return adjacencia;
 }
 
 
 
 long long* dijkstra(int origem) {
+    // std::cout << "origem " << origem << "\n";
+
     std::priority_queue<std::pair<long long int,int>, std::vector<std::pair<long long int,int>>, std::greater<std::pair<long long int,int>> > fila;
-    // ll* distancia = (ll *) Malloc(sizeof(ll) * totalVertices + 1);
-    for (int i = 0; i <= totalVertices; i++) distanciasFinais[origem][i] = INT_MAX;
+
+    ll* distancia = (ll *) Malloc(sizeof(ll) * (totalVertices + 1));
+    for (int i = 0; i <= totalVertices; i++) distancia[i] = LLONG_MAX;
     fila.push({0, origem});
     while (!fila.empty()) {
         std::pair<long long int,int> topo = fila.top();
         long long int peso =  topo.first, v = topo.second;
         fila.pop();
-        if (distanciasFinais[origem][v] < peso) continue;
-        distanciasFinais[origem][v] = peso;
-        for (int i = 1; i <= totalVertices; i++) {
-            if (matrizDoGrafo[v][i] == INT_MAX) continue;
-            if (distanciasFinais[origem][i] > matrizDoGrafo[v][i] + peso){
-                fila.push({peso + matrizDoGrafo[v][i], i});
-                distanciasFinais[origem][i] = matrizDoGrafo[v][i] + peso;
+        if (distancia[v] < peso) continue;
+        distancia[v] = peso;
+        for (ll*& aresta: adjacenciaRebalanceada[v]) {
+            ll vizinho = aresta[0], arestaPeso = aresta[1]; 
+            if (distancia[vizinho] > arestaPeso + peso){
+                fila.push({arestaPeso + peso, vizinho});
+                distancia[vizinho] = arestaPeso + peso;
             }
         }
     }
+    // std::cout << "\n";
+    // std::string output = "origem " + std::to_string(origem) + " ";
+    // for (int i = 1; i <= totalVertices; i++) {
+    //     // std::cout << distancia[i] + (distanciasFalsas[i] - distanciasFalsas[origem]) << " ";
+    //     output += (distancia[i] == LLONG_MAX ? "\u221E" : std::to_string( distancia[i] + distanciasFalsas[i] - distanciasFalsas[origem]) ) + " ";
+    // }
+    // std::cout << output <<  "\n";
+    free(distancia);
     return NULL;
 }
 
@@ -76,56 +95,68 @@ void* thread_task(void* args) {
         }
         proximoVertice--;
         pthread_mutex_unlock(&mutex);
-
         dijkstra(origem);
+        // printf("id %ld, origem %d\n\n" ,id, origem);
+
     }
     pthread_exit(0);
     return (void *) 0;
 
 }
 
-matR novoVertice(int totalVertices, matR adjacencia) {
+matR novoVertice() {
 
-    for (int i = 1; i <= totalVertices; i++)
-        adjacencia.push_back({0, i, 0});
-
+    for (int i = 1; i <= totalVertices; i++) {
+        ll* aresta = (ll*) Malloc(sizeof(ll) * 2);
+        aresta[0] = i; 
+        aresta[1] = 0;
+        adjacencia[0].push_back(aresta);
+    }
     return adjacencia;
 }
 
 
-std::vector<long long> bellman(int origem, int totalVertices, std::vector<std::vector<long long>>& arestas) {
-    std::vector<long long> distancia(totalVertices, 0);
-    for (int i = 1; i <= totalVertices; i++) {
-        for (std::vector<long long>& atual: arestas) {
-            ll u = atual[0], v = atual[1], peso = atual[2];
-            if (distancia[v] > distancia[u] + peso) {
-                distancia[v] = distancia[u] + peso;
+std::vector<long long> bellman() {
+    std::vector<long long> distancia(totalVertices + 1, LLONG_MAX);
+    distancia[0] = 0;
+    for (int i = 0; i <= totalVertices; i++) {
+        for (int u = 0; u <= totalVertices; u++) {
+            for (ll* aresta: adjacencia[u]) {
+                ll v = aresta[0];
+                ll peso = aresta[1];
+                if (distancia[v] > distancia[u] + peso) {
+                    distancia[v] = distancia[u] + peso;
+                }
             }
         }
         // printf("i = %d bellman\n", i);
     }
 
-    for (std::vector<long long>& atual: arestas) {
-        ll u = atual[0], v = atual[1], peso = atual[2];
-        if (distancia[v] > distancia[u] + peso) {
-            return std::vector<ll> (totalVertices, INT_MIN);
+    for (int u = 0; u <= totalVertices; u++) {
+        std::vector<ll*> atual = adjacencia[u];
+        for (ll* aresta: atual) {
+            ll v = aresta[0], peso = aresta[1];
+            if (distancia[v] > distancia[u] + peso) {
+                distancia[v] = distancia[u] + peso;
+                return std::vector<ll> (totalVertices, INT_MIN);
+            }
         }
     }
     return distancia;
 }
 
-matR gerarMatriz(int totalArestas, matR arestas, std::vector<ll> pesos, int totalVertices) {
-
-    matR matrizGrafo = matR (totalVertices + 1, std::vector<ll> (totalVertices + 1, INT_MAX));
-    for (std::vector<ll>& atual: arestas) {
-        int u = atual[0], v = atual[1], p = atual[2];
-        p = p + (pesos[u] - pesos[v]);
-        matrizGrafo[u][v] = p;
-        // printf(" u = %d, v =  %d, p = %d\n", u , v, p);
+ std::vector< std::vector<ll*>> novaAdjacencia() {
+    std::vector< std::vector<ll*>> nova = std::vector< std::vector<ll*> > (totalVertices + 1, std::vector<ll*> {});
+    for (int i = 1 ; i <= totalVertices; i++) {
+        for (ll* aresta: adjacencia[i]) {
+            ll* novaAresta = (ll*) Malloc(sizeof(ll) * 2);
+            novaAresta[0] = aresta[0];
+            novaAresta[1] = aresta[1] + ( distanciasFalsas[i] - distanciasFalsas[aresta[0]]);
+            nova[i].push_back(novaAresta);
+        }
     }
+    return nova;
 
-
-    return matrizGrafo;
 
 }
 
@@ -140,39 +171,41 @@ void printarMatriz(std::vector<std::vector<long long>>& distancias, int totalVer
 }
 
 
-int main() {
+int main(int argc, char** argv) {
 
-    // int totalVertices, totalArestas;
-    // scanf("%d %d", &totalVertices, &totalArestas);
 
-    int totalArestas;
-    scanf("%d %d\n", &totalVertices, &totalArestas);
-    distanciasFinais = (ll**) Malloc(sizeof(ll*) * (totalVertices + 1));
-    for (int i = 1; i <= totalVertices; i++) {
-        distanciasFinais[i] = (ll*) Malloc(sizeof(ll) * (totalVertices + 1));
-    }
-    matR adjacenciaOriginal = lerInput(totalVertices, totalArestas);
+    if (argc != 3) {
+        exitar_erro((char*) "Por favor, execute o programa da seguinte forma:\n./conc <numDeThreads> <nomeDoArquivo>\n\n");
+    } 
+    numThtreads = atoi(argv[1]);
+    if (numThtreads == 0) exitar_erro((char*) "quantidade de threads invalida!!\n");
 
-    matR novaAdjacencia = novoVertice(totalVertices, adjacenciaOriginal);    
+    char* nomeArquivo = argv[2];
     
-    distanciasFalsas = bellman(0, totalVertices + 1, novaAdjacencia);
+    FILE* arq = fopen(nomeArquivo, "r");
 
-    matrizDoGrafo = gerarMatriz(totalArestas, adjacenciaOriginal, distanciasFalsas, totalVertices);
+    if (arq == NULL) exitar_erro((char*) "erro ao abrir arquivo\n");
+
+    // return 0;
 
 
+    fscanf(arq, "%d %d\n", &totalVertices, &totalArestas);
+
+    adjacencia = std::vector< std::vector<ll*> > (totalVertices + 1, std::vector<ll*> {});
+    lerInput(arq);
+    novoVertice();
+
+    distanciasFalsas = std::vector<ll> (totalVertices + 1, 0);
+    // distanciasFalsas = bellman();
+
+    adjacenciaRebalanceada = novaAdjacencia();
     proximoVertice = totalVertices;
+
+
+    double inicio, fim, delta, total = 0;
+    GET_TIME(inicio);
+
     pthread_mutex_init(&mutex, NULL);
-
-
-
-    
-
-
-
-
-    numThtreads = 1;
-    proximoVertice = totalVertices;
-
     pthread_t tids[numThtreads];
 
     for (long long i = 0; i < numThtreads; i++) {
@@ -186,17 +219,12 @@ int main() {
             exitar_erro((char *) "erro ao dar join na thread");
         }
     }
-
-    for (int i = 1; i <= totalVertices; i++) {
-        for (int j = 1; j <= totalVertices; j++) {
-            std::cout << (distanciasFinais[i][j] == INT_MAX ? "\u221E" : std::to_string( distanciasFinais[i][j] + distanciasFalsas[j] - distanciasFalsas[i]) ) + "" << " ";
-        }
-        printf("\n");
-    }
-
-
-
     pthread_mutex_destroy(&mutex);
+    GET_TIME(fim);
+
+
+    printf("%.6f\n", fim - inicio);
+
 
 
     return 0;
---
+++ trabalhoFinal/johnson/script_csv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+EXECUTAVEL=./conc
+
+
+# CSV header
+echo "File,Time_1,Time_2,Time_4,Time_8" > results.csv
+
+# Loop over input files 1.in to 23.in
+for (( i=1; i<=23; i++ ))
+do
+  file="../testes/$i.in"
+  echo "Processing file: $file"
+
+  # Initialize a row with the file name
+  row="$file"
+
+  # For each number of threads, measure the execution time
+  for threads in 1 2 4 8
+  do
+    echo "  Threads: $threads"
+    
+    # Measure the real execution time (in seconds)
+
+    TEMPO=$($EXECUTAVEL $threads $file)
+
+    # Append the execution time to the row
+    row="$row,$TEMPO"
+  done
+
+  # Append the row to the CSV file
+  echo "$row" >> results.csv
+  echo "$row"
+done
